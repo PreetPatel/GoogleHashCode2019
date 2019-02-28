e53b44479ce4ea0e022ad2424f1b09246cd6865b
Preet Patel <iam@preetpatel.com>
2019-03-01 08:00:21 +1300

    Fixed list initialization

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static ArrayList<Photo> HorizontalPhotos;
-    public static ArrayList<Photo> VerticalPhotos;
+    public static ArrayList<Photo> HorizontalPhotos = new ArrayList<>();
+    public static ArrayList<Photo> VerticalPhotos = new ArrayList<>();
 
     public static void main(String[] args) {
         FileParser fileParser = new FileParser(new File("src/Inputs/b_lovely_landscapes.txt"));
